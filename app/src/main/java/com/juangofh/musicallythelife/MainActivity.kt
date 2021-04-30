c8bd9e9cd2bfe5f9c0cedcb237146ded64e794ad
Juan Flores <jgonzalofh2898@gmail.com>
2021-04-30 18:59:48 -0500

    Reformat Code and Optimization Imports

--- app/src/main/java/com/juangofh/musicallythelife/MainActivity.kt
+++ app/src/main/java/com/juangofh/musicallythelife/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.juangofh.musicallythelife
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.juangofh.musicallythelife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
