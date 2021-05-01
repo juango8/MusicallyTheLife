da09f20e8e380e4ae33ae68538c70db50bac6f22
Juan Flores <jgonzalofh2898@gmail.com>
2021-04-30 20:10:44 -0500

    Add a splash screen

--- app/src/main/java/com/juangofh/musicallythelife/MainActivity.kt
+++ app/src/main/java/com/juangofh/musicallythelife/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_MusicallyTheLife)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
