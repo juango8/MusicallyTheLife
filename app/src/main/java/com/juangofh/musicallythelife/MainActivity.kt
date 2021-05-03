adb24dabadf405683f2e95108aa6ce79d049e381
Juan Flores <jgonzalofh2898@gmail.com>
2021-05-02 19:46:19 -0500

    set adapter to viewpager

--- app/src/main/java/com/juangofh/musicallythelife/MainActivity.kt
+++ app/src/main/java/com/juangofh/musicallythelife/MainActivity.kt
@@ -2,8 +2,29 @@ package com.juangofh.musicallythelife
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val NUM_PAGES = 2
 
 class MainActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    title = getString(R.string.title_home)
+                    viewPager.setCurrentItem(0, false)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_playlist -> {
+                    title = getString(R.string.title_playlist)
+                    viewPager.setCurrentItem(1, false)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -12,5 +33,18 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupNavigation()
+        setupViewPager()
+    }
+
+    private fun setupNavigation() {
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    private fun setupViewPager() {
+        val viewPagerAdapter = ViewPagerAdapter(this, NUM_PAGES)
+        viewPager.adapter = viewPagerAdapter
+
     }
 }
---
+++ app/src/main/java/com/juangofh/musicallythelife/ViewPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.juangofh.musicallythelife
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(activity: AppCompatActivity, private val itemsCount: Int) :
+    FragmentStateAdapter(activity) {
+
+    override fun getItemCount() = itemsCount
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> HomeFragment.newInstance()
+            1 -> PlayListFragment.newInstance()
+            else -> HomeFragment.newInstance()
+        }
+    }
+
+}
