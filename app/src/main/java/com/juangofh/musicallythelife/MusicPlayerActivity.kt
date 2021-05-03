f611cc627ff1789924c6ed861ccb78377c4897a7
Juan Flores <jgonzalofh2898@gmail.com>
2021-05-02 21:42:59 -0500

    add music player view

---
+++ app/src/main/java/com/juangofh/musicallythelife/MusicPlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.juangofh.musicallythelife
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MusicPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}
